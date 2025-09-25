a1b0bbd079500004d53116e150ae4d25b8c1165c
Mopuri Guru Shailesh <gurushaileshmopuri@gmail.com>
2025-09-25 12:28:15 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://ec2-18-188-82-67.us-east-2.compute.amazonaws.com
-:8081/api/cart";
+const API_URL = "http://ec2-18-188-82-67.us-east-2.compute.amazonaws.com:8081/api/cart";
 
-const API_BASE = "http://ec2-18-188-82-67.us-east-2.compute.amazonaws.com
-:8081/api/cart";
+const API_BASE = "http://ec2-18-188-82-67.us-east-2.compute.amazonaws.com:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
